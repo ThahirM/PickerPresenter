773347cea8c2c9dbd8c32bed0ecebba32be2d338
Thahir Maheen <thahir@farabi.ae>
2018-05-13 19:03:02 +0400

    Initial commit

---
+++ SamplePresentation/View Controllers/HomeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.swift
+//  SamplePresentation
+//
+//  Created by Thahir Maheen on 5/13/18.
+//  Copyright © 2018 Thahir. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+}
---
+++ SamplePresentation/View Controllers/ModalViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ModalViewController.swift
+//  SamplePresentation
+//
+//  Created by Thahir Maheen on 5/13/18.
+//  Copyright © 2018 Thahir. All rights reserved.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+
+}
+
+class ModalViewController2: UIViewController, PickerPresentable {
+    var pickerHeight: CGFloat {
+        return 250
+    }
+}
+
